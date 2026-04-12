09e576f49da3e212c41fdc28cae15d9f6d506b7b
Samuel Solomon <samuelberihunsolo123@gmail.com>
2026-04-11 21:00:21 -0500

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { connectDB } from './config/db.js';
 import dotenv from "dotenv";
 import rateLimiter from './middleware/rateLimiter.js';
 import cors from "cors";
+import path from 'path';
 
 dotenv.config(); // Load environment variables from .env file to get MONGO_URI which is hidden for security reasons
 const PORT = process.env.PORT || 5001; // Use PORT from environment variables or default to 5001
@@ -11,8 +12,13 @@ const PORT = process.env.PORT || 5001; // Use PORT from environment variables or
 
 const app = express();
 
+const __dirname = path.resolve(); // Get the absolute path of the current directory
 
-app.use(cors()); // Enable CORS for all routes
+if (process.env.NODE_ENV !== "production") {
+
+    app.use(cors()); // Enable CORS for all routes
+
+}
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 
@@ -21,6 +27,15 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 
 
 app.use("/api/trips", tripsRoutes);
+
+if(process.env.NODE_ENV === "production") {
+
+    app.use(express.static(path.join(__dirname, '../frontend/dist'))); // Serve static files from the React build directory
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html')); // Serve index.html for all other routes (for React Router)
+    });
+
+}
 
 
 connectDB().then(() => {
